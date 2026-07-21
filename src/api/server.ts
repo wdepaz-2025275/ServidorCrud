9da87534cd340fac29d006c3ebfd5af3432e8158
wilfred <wdepaz-2025275@kinal.edu.gt>
2026-07-21 11:00:49 -0600

    feat: Se agrego la carpeta api y la clase server

---
+++ src/api/server.ts
@@ -0,0 +1,17 @@
+import { createServer } from "http";
+import { router } from "./router";  
+
+const servidor = createServer(async (req, res) => {
+    await router(req, res);
+});
+
+servidor.listen(3000, () => {
+    
+    console.log("|--------------------------------------------|");
+
+    console.log("| Servidor iniciado)                         |");
+
+    console.log("|http://localhost:3000                       |");
+
+    console.log("|--------------------------------------------|")   
+});
